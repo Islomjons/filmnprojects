fa68fd8dea5015a1eef206d7b12a0ed962f669d2
Islomjons <mukhammadalievislomjon@gmail.com>
2023-06-07 01:05:49 +0500

    I have added Production Sidebar and some css

--- src/components/productionSidebar/ProductionSidebar.jsx
+++ src/components/productionSidebar/ProductionSidebar.jsx
@@ -5,7 +5,9 @@ import { FiX } from "react-icons/fi"
 const ProductionSidebar = ({isProductionSidebarActive, setIsProductionSidebarActive}) => {
   return (
     <div className={isProductionSidebarActive === true ? "productionsidebar activeproductionsidebar" : "productionsidebar"}>
-        <FiX onClick={() => {setIsProductionSidebarActive(false)}}/>
+        <div className='productionISidebar__wrapper'>
+            <FiX onClick={() => {setIsProductionSidebarActive(false)}}/>
+        </div>
     </div>
   )
 }
